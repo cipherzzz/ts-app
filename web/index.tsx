156746d299f2a86c5fbe35412835b4586e622a47
cipherZ <dev@cipherz.com>
2019-01-01 15:57:10 -0500

    removed more web stuff

--- web/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { Api } from "./api/api";
-import { App } from "./app/app";
-import "./index.scss";
-
-Api.Initialize({ host: "localhost:3000", protocol: "http" });
-
-ReactDOM.render(<App />, document.getElementById("app"));
