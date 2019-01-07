e7aa908058c20b9e06b19d90f67752213451d33e
cipherZ <dev@cipherz.com>
2019-01-07 16:48:02 -0500

    deploying to gae - todo: model is not working?

--- api/config/postgres.ts
+++ api/config/postgres.ts
@@ -1,13 +1,21 @@
 import { createConnection, getConnectionOptions } from "typeorm";
 
 export const initializeDbConnection = async () => {
+    // Set defaults
+    const connectionOptions = await getConnectionOptions();
+    Object.assign(connectionOptions, {
+        connection: "postgres",
+        entities: ["api/database/entities/*.ts"],
+        logging: "false",
+        synchronize: "true",
+    });
+
+    // Add Cloudsql required param
     if (process.env.CLOUDSQL) {
-        const connectionOptions = await getConnectionOptions();
         Object.assign(connectionOptions, {
             extra: { socketPath: process.env.CLOUDSQL },
         });
-        return createConnection(connectionOptions);
-    } else {
-        return createConnection();
     }
+
+    return createConnection(connectionOptions);
 };
--- cloudsql/start_proxy.sh
+++ cloudsql/start_proxy.sh
@@ -1,1 +1,1 @@
-./cloud_sql_proxy -instances=vmi-alpha:us-central1:postgresql-default=tcp:5432
+./cloud_sql_proxy -instances=vmi-alpha:us-central1:pg-temp=tcp:5432
