21b071886dd421fc2b0dd119948ce667326d3e26
cipherZ <dev@cipherz.com>
2019-01-09 12:52:24 -0500

    removing docker stuff for dev

--- dev/start-dev.ts
+++ dev/start-dev.ts
@@ -2,11 +2,9 @@ import chokidar from "chokidar";
 import { generateExpressRoutes } from "./steps/gen-routes";
 import { registerQuitKey } from "./steps/register-quit-key";
 import { startApi } from "./steps/start-api";
-import { startDocker } from "./steps/start-docker";
 import { debounce } from "./utils/debounce";
 
 (async () => {
-    await startDocker();
     await Promise.all([generateExpressRoutes().then(() => startApi())]);
 
     const regenerateRoutes = debounce(async (args) => {
